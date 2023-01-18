69b28f4c59efbc317544b5e23d866b60d600757a
Jared Thorne <jthorne@tresearchgroup.tech>
2023-01-17 20:06:19 -0500

    Moved

---
+++ src/main/java/tech/tresearchgroup/schemas/compression/model/CompressionMethodEnum.java
@@ -0,0 +1,16 @@
+package tech.tresearchgroup.schemas.compression.model;
+
+public enum CompressionMethodEnum {
+    BROTLI,
+    BZIP2,
+    DEFLATE,
+    GZIP,
+    LZ4_BLOCK,
+    LZ4_FRAMED,
+    LZMA,
+    SNAPPY_FRAMED,
+    XZ,
+    ZSTD,
+    LZO,
+    SMAZ
+}
